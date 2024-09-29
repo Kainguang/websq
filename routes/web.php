e3f108fe6b5df0614db0987f0efdcf5f25b17197
lraepqearl <suthangrat.s@kkumail.com>
2024-09-29 10:30:50 +0700

    29/9/67 kaimook admin route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\BookingController;
+
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin_CoursesController;
+use App\Http\Controllers\Admin_TrainerController;
+use App\Http\Controllers\Admin_BillController;
+use App\Http\Controllers\Admin_CustomerController;
+use App\Http\Controllers\Admin_FacilitiesController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -52,7 +58,54 @@ Route::middleware(['auth:customer'])->group(function () {
 });
 
 Route::middleware('auth:employee')->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'showAll'])->name('admin_dashboard');
+    // Admin Dashboard Data
+    Route::get('/admin/dashboard', [AdminController::class, 'showDashboard'])->name('admin.dashboard');
+    Route::get('/admin/bill/approve/{id}', [AdminController::class, 'approveBill'])->name('admin.approveBill');
+    Route::get('/admin/trainer', [Admin_TrainerController::class, 'showTrainerDashboard'])->name('admin_trainer');
+    Route::get('/admin/bill', [Admin_BillController::class, 'showbill'])->name('admin_bill');
+    Route::get('/admin/course', [Admin_CoursesController::class, 'showCourseDashboard'])->name('admin_course');
+    Route::get('/admin/customer', [Admin_CustomerController::class, 'showCustomers'])->name('admin_customer');
+    Route::get('/admin/facility', [Admin_FacilitiesController::class, 'showFacilities'])->name('admin_facility');
+
+    // Admin Course Management
+    Route::get('/admin/course/{id}/details', [Admin_CoursesController::class, 'showCourseDetails'])->name('admin.course.details');
+    Route::get('/admin/course/edit/{id}', [Admin_CoursesController::class, 'edit'])->name('courses.edit');
+    Route::post('/admin/course/update/{id}', [Admin_CoursesController::class, 'update'])->name('courses.update');
+    Route::delete('/admin/course/delete/{id}', [Admin_CoursesController::class, 'destroy'])->name('courses.delete');
+
+    // Admin Add Course
+    Route::get('/admin/course/add', [Admin_CoursesController::class, 'showCourseForm'])->name('courses_create');
+    Route::post('/admin/course/store', [Admin_CoursesController::class, 'storeOrUpdate'])->name('courses_store');
+    Route::get('/admin/course/edit/{id}', [Admin_CoursesController::class, 'showCourseForm'])->name('courses_edit');
+    Route::post('/admin/course/update/{id}', [Admin_CoursesController::class, 'storeOrUpdate'])->name('courses_update');
+    Route::get('/admin/course/delete', [Admin_CoursesController::class, 'delete'])->name('courses.delete');
+
+    // Admin Add Trainer
+    Route::get('/admin/trainer/add', [Admin_TrainerController::class, 'showTrainerForm'])->name('trainer_create');
+    Route::post('/admin/trainer/store', [Admin_TrainerController::class, 'storeOrUpdate'])->name('trainer_store');
+    Route::get('/admin/trainer/edit/{id}', [Admin_TrainerController::class, 'showTrainerForm'])->name('trainer_edit');
+    Route::post('/admin/trainer/update/{id}', [Admin_TrainerController::class, 'storeOrUpdate'])->name('trainer_update');
+    Route::get('/admin/trainer/delete', [Admin_TrainerController::class, 'delete'])->name('trainer.delete');
+
+
+    // Admin Add Customer
+    Route::get('/admin/customer/add', [Admin_CustomerController::class, 'showCustomerForm'])->name('customer_create'); // Route สำหรับเพิ่มลูกค้า
+    Route::post('/admin/customer/store', [Admin_CustomerController::class, 'storeOrUpdate'])->name('customer_store'); // Route สำหรับบันทึกข้อมูลลูกค้าใหม่
+    Route::get('/admin/customer/edit/{id}', [Admin_CustomerController::class, 'showCustomerForm'])->name('customer_edit'); // Route สำหรับแก้ไขข้อมูลลูกค้า
+    Route::post('/admin/customer/update/{id}', [Admin_CustomerController::class, 'storeOrUpdate'])->name('customer_update'); // Route สำหรับบันทึกการแก้ไขข้อมูลลูกค้า
+    Route::get('/admin/customer/delete', [Admin_CustomerController::class, 'delete'])->name('customer.delete');
+
+    // Admin bill
+    Route::get('/admin/bill/delete/{id}', [Admin_BillController::class, 'delete'])->name('admin.bill.delete');
+    Route::get('/admin/approve-bill/{id}', [Admin_BillController::class, 'approveBill'])->name('admin.approveBill');
+
+
+    // Admin fac
+    Route::get('/admin/facility/add', [Admin_FacilitiesController::class, 'showFacilityForm'])->name('facility_create');
+    Route::post('/admin/facility/store', [Admin_FacilitiesController::class, 'storeOrUpdate'])->name('facility_store');
+    Route::get('/admin/facility/edit/{id}', [Admin_FacilitiesController::class, 'showFacilityForm'])->name('facility_edit');
+    Route::post('/admin/facility/update/{id}', [Admin_FacilitiesController::class, 'storeOrUpdate'])->name('facility_update');
+    Route::get('/admin/facility/delete/', [Admin_FacilitiesController::class, 'delete'])->name('facility_delete');
 });
 
 // user
@@ -99,7 +152,3 @@ Route::get('/trainer_apply', function () {
     return view('user.trainer_apply');
 })->name('trainer_apply');
 
-//Admin
-Route::get('/admin/course', [AdminController::class, 'showcourse'])->name('admin_course');
-Route::get('/admin/trainer', [AdminController::class, 'showtrainer'])->name('admin_trainer');
-Route::get('/admin/bill', [AdminController::class, 'showbill'])->name('admin_trainer');
