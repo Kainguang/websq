6ccb04d79775f932a0f9c8e68189de37978ff5d3
lraepqearl <suthangrat.s@kkumail.com>
2026-03-08 20:32:04 +0700

    edit course table

--- database/migrations/2024_09_09_115040_create_courses_table.php
+++ database/migrations/2024_09_09_115040_create_courses_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('course_name');
-            $table->string('course_name_th');
             $table->float('course_cost');
             $table->float('course_sellprice');
             $table->string('course_status',1);
